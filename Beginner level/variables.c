2d6f67d797fa69e59b13d330a882df7028c62632
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2017-12-18 15:12:15 +0530

    Create variables.c

---
+++ Beginner level/variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char name[50]="Sachin Ramesh Tendulkar";
+char nickname[50]="Little Master";
+long int runs=18426;
+float ODI=44.83;
+int avg=463;
+printf("%s\n",ch);
+printf("%s\n",nickname);
+printf("%ld\n",runs);
+printf("%f\n",ODI);
+printf("%d\n",avg);
+}
