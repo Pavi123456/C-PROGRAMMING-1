0926cfe03c7360d20a14b72d4443f9f75f0e32bf
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2017-12-19 15:38:11 +0530

    Create palindrome.c

---
+++ codekata/begginer level/palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int n,rev,sum=0,temp;
+    scanf("%d",&n);
+    temp=n;
+    while(temp>0)
+    {
+        rev=temp%10;
+        sum=sum*10+rev;
+        temp/=10;
+    }
+    if(sum==n)
+    {
+        printf("palindrome");
+    }
+    else
+    
+        printf("not a palindrome");
+    
+}
