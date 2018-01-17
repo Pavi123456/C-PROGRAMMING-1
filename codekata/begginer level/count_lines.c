179f07f4a15543c1b1dfbda84654cb20f092e1bd
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-17 21:53:30 +0530

    Update count_lines.c

--- codekata/begginer level/count_lines.c
+++ codekata/begginer level/count_lines.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include<string.h>
-int main() 
-{
-	char a[1000];
-	int n,i,count=1;
-	scanf("%[^\t\n]s",a);
-	n=strlen(a);
-	for(i=0;i<n;i++)
+int main() {
+	char str[1000];
+	int i,l,count=1;
+	scanf("%[^\t\n]s",str);
+	l=strlen(str);
+	for(i=0;i<l;i++)
 	{
-	   if(a[i]=='.'&& a[i+1]!='\0')
-	    {
-	        count++;
-	    }
+	  if(str[i]=='.'&&str[i+1]!='\0')  
+	  {
+	      count++;
+	  }
 	}
 	printf("%d",count);
 	return 0;
 }
-	    
