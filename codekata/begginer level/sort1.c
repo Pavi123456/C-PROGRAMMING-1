0d5d3f09f16e2760d9e045de6f13fedeb71377ff
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-17 23:18:01 +0530

    Update sort1.c

--- codekata/begginer level/sort1.c
+++ codekata/begginer level/sort1.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main(void)
 {
-	int i,a[100],n,temp=0,j;
+	int i,arr[100],n,swap=0,j;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d",&arr[i]);
 	}
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n-i-1;j++)
 		{
-			if(a[j]>a[j+1])
+			if(arr[j]>arr[j+1])
 			{
-				temp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=temp;
+				swap=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=swap;
 			}
 		}
 	}
 	for(i=0;i<n;i++)
 	{
-		printf("%d\n",a[i]);
+		printf("%d\n",arr[i]);
 	}
 }
