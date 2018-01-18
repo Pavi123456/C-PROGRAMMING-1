17371c560c812aeefd349b31cd771b0c9e9dfe91
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-18 22:53:14 +0530

    Create chek_strvowel.java

---
+++ codekata/begginer level/chek_strvowel.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+class check_strvowel{
+	public static void main (String[] args) {
+	Scanner sc=new Scanner(System.in);
+String s=sc.next();
+int i,l,v=0;
+l=s.length();
+for(i=0;i<l;i++)
+{
+    if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
+    {
+        v++;
+    }
+    
+}
+if(v>=1)
+{
+    System.out.print("yes");
+}
+else{
+     System.out.print("no");
+}
+}
+}
