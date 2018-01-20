0beac10453a6257fb27e82c1ccd1bf645c3282cc
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-20 04:37:16 +0000

    Create string_reverse.java

---
+++ CODEKATA/PLAYER LEVEL/string_reverse.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class string_reverse{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+   StringBuffer sb=new StringBuffer(sc.next());
+   StringBuffer rev=sb.reverse();
+   System.out.print(rev);
+}
+}/
