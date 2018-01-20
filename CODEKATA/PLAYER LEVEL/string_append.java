38974136dcafb6b992e9df4812f7a30d0c9fde38
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-20 04:56:13 +0000

    Create string_append.java

---
+++ CODEKATA/PLAYER LEVEL/string_append.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class string_append {
+	public static void main (String[] args) {
+Scanner sc=new Scanner(System.in);
+StringBuffer sb=new StringBuffer(sc.next());
+StringBuffer res=sb.append(".");
+System.out.print(res);
+	}
+}
