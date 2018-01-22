327eaa92dd0941e8a031648cc1b41125540df738
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-22 20:41:16 +0530

    Create concat_string.java

---
+++ codekata/begginer level/concat_string.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1,s2;
+        s1=sc.next();
+        s2=sc.next();
+        System.out.print(s1+s2);
+        
+    }
+}
