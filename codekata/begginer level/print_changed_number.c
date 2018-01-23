f609a58caf6fddcc5aba53369d9482e36e44c79b
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-23 19:41:56 +0530

    Create print_changed_number.c

---
+++ codekata/begginer level/print_changed_number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+int n,i;
+scanf("%d",&n);
+int a[n];
+for(i=1;i<=n;i++)
+{
+    scanf("%d",&a[i]);
+
+    if(a[i]!=i)
+    {
+         printf("%d\n",i);
+    }
+    
+  
+}
+
+}
