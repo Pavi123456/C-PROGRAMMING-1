d61e018aaed08260ba5382b1e3b7ad4365b63112
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-23 20:25:21 +0530

    Create print_in_lexicographical_order.c

---
+++ codekata/begginer level/print_in_lexicographical_order.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	int i,j,temp=0;
+	char s[100];
+	gets(s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		for(j=i+1;s[j]!='\0';j++)
+		{
+			if(s[i]>s[j])
+			{
+				temp=s[i];
+				s[i]=s[j];
+				s[j]=temp;
+			}
+		}
+	}
+	puts(s);
+  return 0;
+}
