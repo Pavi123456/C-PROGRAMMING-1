9162dc15c1ee4b31c125a2b309313bda06b3ee47
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-02-01 19:58:26 +0530

    Create holiday.java

---
+++ CODEKATA/PLAYER LEVEL/holiday.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class a{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    String S=sc.nextLine();
+    if(S.equalsIgnoreCase("Saturday"))
+    {
+        System.out.print("Yes");
+    }
+    else if(S.equalsIgnoreCase("Sunday"))
+    {
+        System.out.print("Yes");
+    }
+    else
+    {
+        System.out.print("No");
+    }
+}
+}
