4c59e7149852f291fd743561d1f6d7ce2f005763
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-05-16 23:44:27 +0530

    Update largestnumber.java

--- CODEKATA/HUNTER LEVEL/largestnumber.java
+++ CODEKATA/HUNTER LEVEL/largestnumber.java
@@ -2,7 +2,8 @@ import java.util.*;
 class largestnumber{  
  public static void main(String args[]){  
   Scanner sc=new Scanner(System.in);
-  int n,i,j,temp;
+  HashSet <Integer> set = new HashSet <Integer>();
+  int n,i,j,temp,count=0;
   n=sc.nextInt();
   int a[]=new int[n];
     int b[]=new int[n];
@@ -11,6 +12,19 @@ class largestnumber{
       a[i]=sc.nextInt();
       
   }
+  for(i=1;i<n;i++)
+  {
+  if(a[0]==a[i])
+  {
+      count++;
+  }
+  }
+   
+  if(count==n-1)
+  {
+    System.out.println(a[1]);  
+  }
+  else{
    for(i=0;i<n;i++)
    {
        for(j=i+1;j<n;j++)
@@ -23,9 +37,13 @@ class largestnumber{
           }
        }
    }
+  
+  
+ 
    for(i=0;i<n;i++)
    {
        System.out.print(a[i]);
    }
+  }
  }
 }
