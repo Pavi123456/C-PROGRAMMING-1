96fabda40a1d947f1b2fc048d4b621eee10da0b2
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-05-16 22:57:03 +0530

    Create duplicate_element.java

---
+++ CODEKATA/HUNTER LEVEL/duplicate_element.java
@@ -0,0 +1,35 @@
+import java.util.*;  
+class unique{  
+ public static void main(String args[]){  
+  
+  HashSet<Integer> set=new HashSet<Integer>(); 
+  Scanner sc=new Scanner(System.in);
+  int n,i,j,t=0;
+  n=sc.nextInt();
+  int a[]=new int[n];
+    int b[]=new int[n];
+  for(i=0;i<n;i++)
+  {
+      a[i]=sc.nextInt();
+      
+  }
+   for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+           if(a[i]==a[j])
+           {
+             
+               b[t]=a[i];
+               t++;
+               break;
+           }
+       }
+   }
+  
+   for(i=0;i<t;i++)
+   {
+   System.out.print(b[i]+" ");  }
+  }  
+ }  
+ 
