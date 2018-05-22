bf37465506c412b451cbffb6f6059aa112e1154b
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-05-22 23:16:11 +0530

    Create maxsubarray_29.java

---
+++ CODEKATA/HUNTER LEVEL/maxsubarray_29.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class maxsubarray 
+{
+	public static void main(String[] args)
+	{
+		int n,sum=0,i,max,j;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=scan.nextInt();
+		}
+	    max=a[0];
+		for(i=0;i<n;i++)
+		{	 
+		  sum=0;
+		  sum=a[i];
+		  for(j=i+1;j<n;j++)
+		  {
+			sum=sum+a[j];
+			
+			if(sum>max)
+			   max=sum;
+		  }
+		}
+		System.out.print(max);
+	}
+}
