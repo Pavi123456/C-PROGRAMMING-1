38082eae76841cc431560de1986e9662bbda44dc
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-06-26 21:43:12 +0530

    Create rightangledtriangle_124.java

---
+++ CODEKATA/HUNTER LEVEL/SET 13/rightangledtriangle_124.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class rightangledtriangle{
+        public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i,j;
+        for(i=n;i>0;i--)
+        {
+            for(j=i;j>0;j--)
+            {
+            
+                System.out.print("1"+" ");
+                
+            
+            }
+            System.out.println(" ");
+        }
+    }
+}
+   
