5abfe59cd4cfa4e67ca649b57d57c2c298277245
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-07-03 12:32:11 +0530

    Create stringpos_107.java

---
+++ CODEKATA/PLAYER LEVEL/SET-11/stringpos_107.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+class stringpos{
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int i;
+	    String s=sc.nextLine();
+	    String s1=sc.nextLine();
+	    String w[]=s.split(" ");
+	    
+	    for(i=0;i<w.length;i++)
+	    {
+	        if(w[i].equals(s1))
+	        {
+	            System.out.print(i);
+	            break;
+	        }
+	    }
+	    
+	    
+	}
+}
