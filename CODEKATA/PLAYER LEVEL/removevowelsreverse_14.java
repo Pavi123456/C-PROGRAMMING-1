407b3abb909d3fd87753df218c8afa3d4baac883
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-07-09 20:04:50 +0530

    Update removevowelsreverse_14.java

--- CODEKATA/PLAYER LEVEL/removevowelsreverse_14.java
+++ CODEKATA/PLAYER LEVEL/removevowelsreverse_14.java
@@ -4,11 +4,12 @@ import java.util.*;
 class removevowelsreverse{
 	public static void main (String[] args) {
 	Scanner sc=new Scanner(System.in);
+	int l=sc.nextInt();
 	String s=sc.nextLine();
     char c[]=s.toCharArray();
     String v="";
-    int i,l;
-    l=s.length();
+    int i;
+    
     for(i=0;i<l;i++)
     {
         if(c[i]!='a'&&c[i]!='e'&&c[i]!='i'&&c[i]!='o'&&c[i]!='u'&&c[i]!='A'&&c[i]!='E'&&c[i]!='I'&&c[i]!='O'&&c[i]!='U')
